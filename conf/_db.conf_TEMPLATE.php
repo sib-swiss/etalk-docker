f33826809243318e7a31422692661b0d01e76045
jdugied <jean.dugied@sib.swiss>
2020-03-25 17:19:10 +0100

    conf template

--- conf/_db.conf_TEMPLATE.php
+++ conf/_db.conf_TEMPLATE.php
@@ -1,14 +1,14 @@
 <?php
 
 if ($_SERVER['HTTP_HOST']=='127.0.0.1'||$_SERVER['HTTP_HOST']=='etalk.cc') {
-	$GLOBALS['DBHost'] = 'localhost';
-	$GLOBALS['DBUser'] = 'php';
-	$GLOBALS['DBPass'] = 'polipo98';
+	$GLOBALS['DBHost'] = 'XXXX';
+	$GLOBALS['DBUser'] = 'XXXX';
+	$GLOBALS['DBPass'] = 'XXXX';
 }
 else {
 	$GLOBALS['DBHost'] = 'localhost';
-	$GLOBALS['DBUser'] = 'root';
-	$GLOBALS['DBPass'] = 'root';
+	$GLOBALS['DBUser'] = 'XXXX';
+	$GLOBALS['DBPass'] = 'XXXX';
 }
 $GLOBALS['DBName'] = 'etalk2';
 
