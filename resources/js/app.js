eb84512683ef8587b996d1744edb8088be187d65
Silvano Aldà <silvano.alda@sib.swiss>
2022-10-04 16:05:25 +0200

    etalk show: implmented play,stop,next,prev, tiggleMute

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,128 @@
-require('./bootstrap');
+import "./bootstrap";
 
+import "tw-elements";
 
-import 'tw-elements';
-
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 window.Alpine = Alpine;
+
+Alpine.data("etalkShow", (audioFiles = []) => ({
+    status: null,
+    wait: true,
+    audioFiles: audioFiles,
+    currentSnd: 0,
+    init() {
+        // console.log("I am called automatically");
+        setTimeout(() => {
+            this.play();
+        }, 300);
+    },
+
+    play() {
+        this.wait = false;
+        this.status = "play";
+
+        this.$refs.player.play();
+        // $("#bPlay").hide();
+        // document.getElementById("bPause").style.display = "inline";
+        // player.play();
+        // $("body.viewer").removeClass("paused");
+        // $("#wait").fadeOut(function () {
+        // });
+    },
+    stop() {
+        this.wait = true;
+        this.status = "pause";
+        this.$refs.player.pause();
+    },
+    pause() {
+        this.status = "pause";
+        this.$refs.player.pause();
+    },
+    canplay() {
+        console.log("canplay");
+        this.status = "canplay";
+        // document.getElementById('loading').style.display='none';"
+    },
+    loadstart() {
+        console.log("loadstart");
+        // document.getElementById('loading').style.display='inline';"
+    },
+    startedPlay() {
+        console.log("startedPlay");
+    },
+    endedPlay() {
+        console.log("endedPlay");
+        this.next();
+    },
+    errorHandler() {
+        console.log("errorHandler");
+        // alert('The sound file \''+this.src+'\' could not be loaded.')
+    },
+    toggleMute() {
+        this.$refs.player.muted = !this.$refs.player.muted;
+        this.$el.src =
+            "/i/audio_" + (this.$refs.player.muted ? "on" : "mute") + ".png";
+    },
+
+    next() {
+        if (this.currentSnd < this.audioFiles.length - 1) {
+            this.setCurrentSnd(parseInt(this.currentSnd, 10) + 1);
+            this.play();
+            return;
+        }
+
+        alert("End of the track.");
+    },
+
+    prev() {
+        if (this.currentSnd > 0) {
+            this.setCurrentSnd(parseInt(this.currentSnd, 10) - 1);
+            this.play();
+            return;
+        }
+
+        alert("Start of the track.");
+    },
+
+    setCurrentSnd(soundIndex) {
+        this.currentSnd = soundIndex;
+        location.hash = soundIndex;
+        this.$refs.player.src =
+            "/storage/data/" + audioFiles[this.currentSnd].name;
+
+        // set current class in relative VIZ > div
+
+        this.$refs.suondFigure.src =
+            "/storage/tmp/" + audioFiles[this.currentSnd].file;
+
+        // currentSnd = e;
+        // location.hash = e;
+        // player.src = "/data/" + audioFiles[currentSnd].snd;
+        // $("#viz a").removeClass("current");
+        // $("#a" + currentSnd).addClass("current");
+        // "/tmp/" + audioFiles[currentSnd]["pict"] != $("#diaPict").attr("src") && $("#dia>figure").fadeOut(kFadeDuration, function () {
+        //     var e;
+        //     if (audioFiles[currentSnd].pict !== "") {
+        //         $("#diaPict").attr("src", "/tmp/" + audioFiles[currentSnd].pict);
+        //         e = "";
+        //         audioFiles[currentSnd].pict_cred.length > 0 ? e += "© " + audioFiles[currentSnd].pict_cred : e += audioFiles[currentSnd].pict_cred;
+        //         audioFiles[currentSnd]["pict_link"] != "" && (e += '<br/><a href="' + audioFiles[currentSnd].pict_link + '" target="_blank">' + audioFiles[currentSnd].pict_link + "</a>");
+        //         $("#dia>figure>figcaption").html(e);
+        //         $("#dia>figure").fadeIn()
+        //     }
+        // });
+        // $("#links").fadeOut(kFadeDuration, function () {
+        //     if (audioFiles[currentSnd].link !== "") {
+        //         $("#links").html(audioFiles[currentSnd].link);
+        //         $("#links").fadeIn()
+        //     }
+        // });
+        // $("#links a.entity").bind("click touchstart", function (e) {
+        //     e.preventDefault();
+        //     e.stopPropagation();
+        //     openLinkinTab($(this).attr("href"))
+        // })
+    },
+}));
+
 Alpine.start();
--- resources/views/talk.blade.php
+++ resources/views/talk.blade.php
@@ -1,71 +1,138 @@
 @extends('layouts.app')
 
 @section('content')
-    <header class="fixed w-full bg-white flex justify-between py-2 px-4 ">
-        @if (!request()->embed)
-            <nav class="opacity-30">
-                <a href="{{ route('home') }}">
-                    <img src="{{ url('i/back.png') }}" id="bBack" alt="Back" class="btn"
-                        title="Retour à l’accueil" />
-                </a>
+    <div x-data="etalkShow({{ $talk->sounds->map->only('file', 'name') }})">
+
+        <div id="wait" :class=" wait ? 'flex' : 'hidden'">
+            <header>
+                <h1>{{ $talk->title }}</h1>
+                <h2>{{ $talk->author }} - {{ $talk->date->format('d.m.Y') }}</h2>
+                <a @click="play()" href="#0" class="vidPlay">▶</a>
+            </header>
+            <nav>
+                <h2>Sommaire</h2>
+                @foreach ($talk->sounds as $i => $sound)
+                    @if ('section' === $sound->chaptering)
+                        <a href="#{{ $i }}">{{ $sound->section_title }}</a>
+                    @endif
+                @endforeach
             </nav>
-        @endif
-
-        <nav id="controls" class="flex opacity-30">
-            <img src="{{ url('i/loading.gif') }}" id="loading" class="btn" alt="" />
-            <img src="{{ url('i/share.png') }}" id="bShare" class="btn" alt="Share"
-                title="Partager / Intégrer" />
-            <img src="{{ url('i/audio_mute.png') }}" id="bMute" class="btn" alt="Mute"
-                title="Activer/Couper le son" />
-            <img src="{{ url('i/mode_full.png') }}" id="bMode" class="btn" alt="Transcript"
-                title="Afficher/Masquer le transcript" />
-            <img src="{{ url('i/prev.png') }}" id="bPrev" class="btn" alt="◀︎◀︎" title="Précédent" />
-            <img src="{{ url('i/play.png') }}" id="bPlay" class="btn" alt="▶︎" title="Play" />
-            <img src="{{ url('i/pause.png') }}" id="bPause" class="btn" alt="Pause" title="Pause" />
-            <img src="{{ url('i/stop.png') }}" id="bStop" class="btn" alt="◼︎" title="Stop" />
-            <img src="{{ url('i/ffw.png') }}" id="bNext" class="btn" alt="▶︎▶︎" title="Suivant" />
-        </nav>
-    </header>
+        </div>
 
 
 
-    <div style="background-image: url('{{ url('i/bg_dia.png') }}') " class="flex">
+        {{-- <aside id="embed">
+        <div>
+            <div class="close">×</div>
+            <h1>Intégrer cette présentation</h1>
+            <input id="fShareURL" type="text" readonly="readonly"
+                style="float:right;width:87%;border:1px solid #000;margin-top:-1px;">
+            <label>URL:</label>
+            <br>
+            <textarea id="embed_code" readonly="readonly"></textarea>
+            <form id="embed_customize" action="/">
+                <fieldset>
+                    <legend>Dimensions :</legend>
+                    <div><input id="embed_w" type="text" value="720"> × <input id="embed_h" type="text"
+                            value="405"> pixels</div>
+                </fieldset>
+                <fieldset>
+                    <legend>Options:</legend>
+                    <ul>
+                        <li>
+                            <input id="embed_desc" type="checkbox" checked="checked"><label for="embed_desc"> Description
+                                sous la vidéo</label>
+                        </li>
+                        <li>
+                            <input id="embed_link" type="checkbox" checked="checked"><label for="embed_link"> Lien permanent
+                                dans la description</label>
+                        </li>
+                    </ul>
+                </fieldset>
+            </form>
+        </div>
+    </aside> --}}
 
-        <div class="w-1/4 bg-white p-6">
-            @foreach ($talk->sounds as $sound)
-                <div class="pb-6">
-                    <h2 class="text-2xl font-bold mb-6">{{ $sound->section_title }}</h2>
-                    {{ $sound->text }}
+
+        <div>
+            <header class="fixed w-full bg-white flex justify-between py-2 px-4 shadow-xl">
+                @if (!request()->embed)
+                    <nav class="opacity-30">
+                        <a href="{{ route('home') }}">
+                            <img src="{{ url('i/back.png') }}" id="bBack" alt="Back" class="btn"
+                                title="Retour à l’accueil" />
+                        </a>
+                    </nav>
+                @endif
+
+                <nav id="controls" class="flex opacity-30">
+                    <img src="{{ url('i/loading.gif') }}" :class=" 'loading' == status ? 'inline' : 'hidden'" class="btn"
+                        alt="" />
+
+                    <img src="{{ url('i/share.png') }}" id="bShare" class="btn" alt="Share"
+                        title="Partager / Intégrer" />
+
+                    <img src="{{ url('i/audio_mute.png') }}" ù class="btn" alt="Mute" @click="toggleMute"
+                        title="Activer/Couper le son" />
+
+                    <img src="{{ url('i/mode_full.png') }}" id="bMode" class="btn" alt="Transcript"
+                        title="Afficher/Masquer le transcript" />
+
+                    <img src="{{ url('i/prev.png') }}" @click="prev" class="btn" alt="◀︎◀︎" title="Précédent" />
+
+                    <img src="{{ url('i/play.png') }}" :class=" 'play' == status ? 'hidden' : 'inline'" class="btn"
+                        @click="play" alt="▶︎" title="Play" />
+
+                    <img src="{{ url('i/pause.png') }}" :class="  'play' == status ? 'inline' : 'hidden'" @click="pause"
+                        class="btn" alt="Pause" title="Pause" />
+
+                    <img src="{{ url('i/stop.png') }}" @click="stop" class="btn" alt="◼︎" title="Stop" />
+
+                    <img src="{{ url('i/ffw.png') }}" @click="next" class="btn" alt="▶︎▶︎" title="Suivant" />
+                </nav>
+            </header>
+
+
+
+            <div style="background-image: url('{{ url('i/bg_dia.png') }}') " class="flex">
+
+                <div id="viz" x-ref="viz" class="w-1/4 bg-white p-6">
+                    @foreach ($talk->sounds as $i => $sound)
+                        <div class="sound" :class=" currentSnd == {{ $i }} ? 'current' : ''"
+                            @click="setCurrentSnd({{ $i }}); play()">
+                            @if ($sound->chaptering === 'section')
+                                <h2>{{ $sound->section_title }}</h2>
+                            @endif
+                            {{ $sound->text }}
+                        </div>
+                    @endforeach
                 </div>
-            @endforeach
-
-        </div>
 
 
-        <div class="w-3/4 p-4 flex flex-col justify-between h-screen fixed left-1/4 top-10">
-            <div class="text-center ">
-                <figure class="inline-block ">
-                    <img src="{{ url('storage/tmp/' . $talk->sounds[0]->file) }}" alt="">
-                    <figcaption></figcaption>
-                </figure>
+                <div class="w-3/4 p-4 flex flex-col justify-between h-screen fixed left-1/4 top-10">
+                    <div class="text-center ">
+                        <figure class="inline-block ">
+                            <img x-ref="suondFigure" src="{{ url('storage/tmp/' . $talk->sounds[0]->file) }}"
+                                alt="">
+                            <figcaption></figcaption>
+                        </figure>
+                    </div>
+                    <div class="bg-white mb-10 rounded-sm p-4 flex">
+                        <span>References</span>
+                        <a class="flex" href="{{ $talk->sounds[0]->entities }}">
+                            <img src="{{ url('i/link.png') }}" alt="">
+                            https:
+                        </a>
+                    </div>
+                </div>
+
+
+                <audio x-ref="player" preload="auto" src="{{ url('storage/data/' . $talk->sounds[0]->name) }}"
+                    x-on:error="errorHandler" x-on:ended="endedPlay" x-on:loadstart="loadstart" x-on:canplay="canplay"
+                    x-on:play="startedPlay">
+                    <source src="{{ url('storage/data/' . $talk->sounds[0]->name) }}" type="audio/mp3">HTML5 Only!
+                </audio>
             </div>
-
-
-            <div class="bg-white mb-10 rounded-sm p-4 flex">
-                <span>References</span>
-                <a class="flex" href="{{ $talk->sounds[0]->entities }}">
-                    <img src="{{ url('i/link.png') }}" alt="">
-                    https:
-                </a>
-            </div>
         </div>
-
-
-        <audio id="player" preload="auto" src="{{ url('storage/data/' . $talk->sounds[0]->name) }}"
-            onerror="alert('The sound file \''+this.src+'\' could not be loaded.');" onended="endedPlay();"
-            onloadstart="document.getElementById('loading').style.display='inline';"
-            oncanplay="document.getElementById('loading').style.display='none';" onplay="startedPlay();">
-            <source src="{{ url('storage/data/' . $talk->sounds[0]->name) }}" type="audio/mp3">HTML5 Only!
-        </audio>
     </div>
 @endsection
