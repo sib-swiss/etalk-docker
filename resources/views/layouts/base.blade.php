6b0664e190f6794360e4c706d1cb3da5d476ed97
Silvano Aldà <silvano.alda@sib.swiss>
2022-10-04 11:08:32 +0200

    moved  from mix to vite

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @hasSection('title')
-
         <title>@yield('title') - {{ config('app.name') }}</title>
     @else
         <title>{{ config('app.name') }}</title>
@@ -17,12 +16,7 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ url(mix('css/app.css')) }}">
-    @livewireStyles
-
-    <!-- Scripts -->
-    <script src="{{ url(mix('js/app.js')) }}" defer></script>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,19 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-            },
-        },
+        extend: {},
     },
-    variants: {
-        extend: {
-            backgroundColor: ['active'],
-        }
-    },
+
     content: [
-        './app/**/*.php',
-        './resources/**/*.html',
-        './resources/**/*.js',
-        './resources/**/*.jsx',
-        './resources/**/*.ts',
-        './resources/**/*.tsx',
-        './resources/**/*.php',
-        './resources/**/*.vue',
-        './resources/**/*.twig',
-        './node_modules/tw-elements/dist/js/**/*.js'
+        "./app/**/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./node_modules/tw-elements/dist/js/**/*.js",
     ],
     plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-        require('tw-elements/dist/plugin')
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+        require("tw-elements/dist/plugin"),
     ],
 };
