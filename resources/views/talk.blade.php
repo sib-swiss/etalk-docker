7c9472f8861364b65593a46811cd0a0d92caa0f5
Silvano Aldà <silvano.alda@sib.swiss>
2022-12-13 15:07:06 +0100

    etalk show: share button

--- resources/views/talk.blade.php
+++ resources/views/talk.blade.php
@@ -25,40 +25,6 @@
         </div>
 
 
-
-        {{-- <aside id="embed">
-        <div>
-            <div class="close">×</div>
-            <h1>Intégrer cette présentation</h1>
-            <input id="fShareURL" type="text" readonly="readonly"
-                style="float:right;width:87%;border:1px solid #000;margin-top:-1px;">
-            <label>URL:</label>
-            <br>
-            <textarea id="embed_code" readonly="readonly"></textarea>
-            <form id="embed_customize" action="/">
-                <fieldset>
-                    <legend>Dimensions :</legend>
-                    <div><input id="embed_w" type="text" value="720"> × <input id="embed_h" type="text"
-                            value="405"> pixels</div>
-                </fieldset>
-                <fieldset>
-                    <legend>Options:</legend>
-                    <ul>
-                        <li>
-                            <input id="embed_desc" type="checkbox" checked="checked"><label for="embed_desc"> Description
-                                sous la vidéo</label>
-                        </li>
-                        <li>
-                            <input id="embed_link" type="checkbox" checked="checked"><label for="embed_link"> Lien permanent
-                                dans la description</label>
-                        </li>
-                    </ul>
-                </fieldset>
-            </form>
-        </div>
-    </aside> --}}
-
-
         <div>
             <header class="fixed w-full bg-white flex justify-between py-2 px-4 shadow-xl top-0 z-10">
                 @if (!request()->embed)
@@ -74,8 +40,8 @@
                     <img src="{{ url('i/loading.gif') }}" :class=" 'loading' == status ? 'inline' : 'hidden'" class="btn"
                         alt="" />
 
-                    <img src="{{ url('i/share.png') }}" id="bShare" class="btn" alt="Share"
-                        title="Partager / Intégrer" />
+                    <img src="{{ url('i/share.png') }}" id="bShare" class="btn" alt="Share" data-bs-toggle="modal"
+                        data-bs-target="#shareUrlModal" title="Partager / Intégrer" />
 
                     <img src="{{ url('i/audio_mute.png') }}" class="btn" alt="Mute" @click="toggleMute"
                         title="Activer/Couper le son" />
@@ -144,6 +110,34 @@
                     x-on:play="startedPlay">
                     <source src="{{ url('storage/data/' . $talk->sounds[0]->name) }}" type="audio/mp3">HTML5 Only!
                 </audio>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    <!-- Modal SHARE -->
+    <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
+        id="shareUrlModal" tabindex="-1" aria-labelledby="shareUrlModalLabel" aria-hidden="true">
+        <div class="modal-dialog relative w-auto pointer-events-none">
+            <div
+                class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+
+                <div class="modal-body relative p-4">
+
+                    <div class="flex items-center">
+                        <div class="pr-3">URL:</div>
+                        <div class="grow">
+                            <input class="w-full" type="text" value="{{ url()->current() }}" readonly="readonly">
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
         </div>
     </div>
