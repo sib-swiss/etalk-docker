f801eaf061d2216bfe96b1419c93d20ad695f0d2
Silvano Aldà <silvano.alda@sib.swiss>
2023-02-22 12:48:08 +0100

    CI

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -22,11 +22,6 @@
     $app_dir = $server === 'prod' ?  $deploy_path_prod :  $deploy_path_dev ;
     
     $server = isset($server) ? $server : 'dev' ;
-    var_dump([
-        $server,
-        $server_dev 
-    ]);
-    exit();
 @endsetup
 
 
